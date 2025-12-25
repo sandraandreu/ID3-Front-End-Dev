4ec8e797205755316a400f767311e8c545e417b3
sandraandreu <sandreum08@gmail.com>
2025-12-25 19:24:33 +0100

    Avances

--- Ejercicios Navidad/perricos-id3-main/index.js
+++ Ejercicios Navidad/perricos-id3-main/index.js
@@ -1,6 +1,7 @@
 const perricosArray = [];
 
-// addPerrico();
+
+// Definición de nombres de perros
 
 const dogName = ["Sucky","Drako","Pitter","Frida","Rocky","Neu","Body","Chena","Ghost", "Fiona", "Balto", "Canela", "Chispa", "Rocco", "Sombra"]
 
@@ -8,6 +9,63 @@ function randomPerritoName () {
     const randomIndex = Math.floor(Math.random() * dogName.length);
     return dogName[randomIndex];
 }
+
+// Añadir perritos al pulsar los botones
+
+  // Añadir 1 perrito
+
+  const addPerrico = async () => {
+    const perricoImg = await getRandomDogImage();
+    const name = randomPerritoName();
+    const perrico = {
+      name,
+      perricoImg
+    }
+
+    perricosArray.push(perrico);
+    renderPerricoArray();
+  };
+
+  renderPerricoArray();
+
+  document.querySelector('#add-1-perrico').addEventListener('click', function () {
+    addPerrico();
+  });
+
+  // Añadir 5 perricos
+  
+  function add5Perrico () {
+    for(let counter = 1; counter <= 5; counter++) {
+    addPerrico();}
+  }
+
+  document.querySelector('#add-5-perrico').addEventListener('click', function () {
+    add5Perrico();
+  });
+
+// Card del perrito en la interfaz
+
+function renderPerricoArray() {
+  const dogList = document.querySelector('#dog-list');
+  dogList.innerHTML = '';
+
+  perricosArray.forEach((dog, index) => {
+    
+    const htmlAdd = `<div class="card">
+      <img src="${dog}" alt="Perro" />
+      <br />
+      <h2>${dog.name}</h2>
+      <p>❤️ 🤮</p>
+      <button>Preciosísimo</button> <button>Feísisimo</button>
+    </div>`;
+
+    console.log('innerHtml posición', index, dogList.innerHTML);
+
+    dogList.innerHTML += htmlAdd;
+  });
+}
+
+// Filtros por nombres
 
 function renderPerricoFilter() {
   const dogFilterName = document.querySelector('#dog-filter-name');
@@ -34,49 +92,5 @@ function renderPerricoFilter() {
 
 renderPerricoFilter()
 
-function renderPerricoArray() {
-  const dogList = document.querySelector('#dog-list');
-  dogList.innerHTML = '';
-
-  perricosArray.forEach((dogImage, index) => {
-    
-    const htmlAdd = `<div class="card">
-      <img src="${dogImage}" alt="Perro" />
-      <br />
-      <h2>${randomPerritoName()}</h2>
-      <p>❤️ 🤮</p>
-      <button>Preciosísimo</button> <button>Feísisimo</button>
-    </div>`;
-
-    console.log('innerHtml posición', index, dogList.innerHTML);
-
-    dogList.innerHTML += htmlAdd;
-  });
-}
-
-const addPerrico = async () => {
-  const perricoImg = await getRandomDogImage();
-  perricosArray.push(perricoImg);
-  renderPerricoArray();
-
-  
-};
-
-renderPerricoArray();
-
-document.querySelector('#add-1-perrico').addEventListener('click', function () {
-  addPerrico();
-});
 
 
-
-function add5Perrico () {
-  for(let counter = 1; counter <= 5; counter++) {
-	addPerrico();}
-}
-
-
-
-document.querySelector('#add-5-perrico').addEventListener('click', function () {
-  add5Perrico();
-});

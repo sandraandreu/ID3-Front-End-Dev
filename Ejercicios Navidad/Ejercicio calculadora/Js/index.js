1c525d32bff485ee74b0d464c6a22b0a443b68ac
sandraandreu <sandreum08@gmail.com>
2026-01-08 22:52:37 +0100

    avances

--- Ejercicios Navidad/Ejercicio calculadora/Js/index.js
+++ Ejercicios Navidad/Ejercicio calculadora/Js/index.js
@@ -1,5 +1,10 @@
 // Calculadora en la interfaz
 
+let result = "0";
+let num1 = "0";
+let num2 = "0";
+let operation;
+const operators = ["+", "−", "÷", "×"];
 const calculatorKeys = [
   "AC",
   "DEL",
@@ -22,10 +27,8 @@ const calculatorKeys = [
   "=",
 ];
 
-let result = 0;
-
-function renderCalculator() {
-  // Teclas
+// Teclas
+function renderCalculatorKeys() {
   const keys = document.querySelector("#keys");
   keys.innerHTML = "";
 
@@ -34,8 +37,12 @@ function renderCalculator() {
 
     keys.innerHTML += htmlAddKeys;
   });
+}
 
-  //Resultado
+renderCalculatorKeys();
+
+//Resultado
+function renderCalculatorResult() {
   const resultInterface = document.querySelector("#result");
   resultInterface.innerHTML = "";
 
@@ -44,4 +51,147 @@ function renderCalculator() {
   resultInterface.innerHTML = htmlAddResult;
 }
 
-renderCalculator();
+renderCalculatorResult();
+
+// Pulsar botones
+
+const buttons = document.querySelectorAll(".key");
+
+let buttonClicked;
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    buttonClicked = event.target.textContent;
+    console.log(`Has pulsado ${buttonClicked}`);
+
+    function clickButtons() {
+        if (buttonClicked == "AC") {
+            clickAC()
+        }
+        else if (buttonClicked=="DEL") {
+            clickDEL()
+        }
+        else if (!isNaN(Number(buttonClicked))) {
+            clickNumbers()
+        }
+        else if (!result.includes(".") && buttonClicked == ".") {
+            clickPoint()
+        }
+        else if (!result.includes("%") && buttonClicked == "%") {
+            clickPercentatge
+        }
+        else if (buttonClicked == "=" || !num2 == 0 && operation) {
+            operate()
+        }
+    }
+
+      
+
+      else if (buttonClicked == "=") {
+        
+      }
+
+      // Clicar operaciones
+      else if (operators.includes(buttonClicked)) {
+        if (num2 == "0") {
+            operation = buttonClicked;
+        }
+        else {
+
+        }
+      }
+    }
+
+    operations();
+    renderCalculatorResult();
+  });
+});
+
+// voy pulsando numero entonces el resultado es el num1 y el num2 es 0
+// cuando pulso una operacion se guarda en la operacion y el resultado sigue siendo el num1 y num2 es 0
+//si la operacion NO esta vacia los numeros que vaya escribiendo se iran guardando en numero 2
+// cuando pulse el igual el resultado sera num1 (operacion) num2 y num2 pasara a estar en 0 otra vez
+
+
+//Funciones de los botones 
+
+    // Reset
+    function clickAC () {
+    num1 = 0
+    result = num1
+    }
+
+    // Borrar
+    function clickDEL () {
+    if (result == "0") {
+        result = "0";
+    } else if (result.length > 1) {
+        result = result.slice(0, result.length - 1);
+    } else {
+        result = "0";
+    }
+    }
+
+    // Clicar numeros
+    function clickNumbers () {
+    if (result == "0") {
+        num1 = buttonClicked;
+        result = num1;
+    } else if (!operation) {
+        num1 += buttonClicked;
+        result = num1;
+    } else if (num2 == "0") {
+        num2 = buttonClicked;
+        result = num2;
+    } else {
+        num2 += buttonClicked;
+        result = num2;
+    }
+    }
+
+    // Clicar "."
+    function clickPoint () {
+    if (num2 == 0) {
+        num1 += ".";
+        result = num1;
+    } else {
+        num2 += ".";
+        result = num2;
+    }
+    }
+
+    // Clicar "%"
+    function clickPercentatge () {
+            if (num2 == "0") {
+            num1 += "%";
+            result = num1;
+            } else {
+            num2 += "%";
+            result = num2;
+            }
+        }
+
+    // Operar
+      function operate () {
+        if (operation == "+") {
+          num1 = Number(num1) + Number(num2);
+          result = num1;
+          num2 = 0;
+          operation = "";
+        } else if (operation == "−") {
+          num1 = Number(num1) - Number(num2);
+          result = num1;
+          num2 = 0;
+          operation = "";
+        } else if (operation == "÷") {
+          num1 = Number(num1) / Number(num2);
+          result = num1;
+          num2 = 0;
+          operation = "";
+        } else if (operation == "×") {
+          num1 = Number(num1) * Number(num2);
+          result = num1;
+          num2 = 0;
+          operation = "";
+        }
+      }
